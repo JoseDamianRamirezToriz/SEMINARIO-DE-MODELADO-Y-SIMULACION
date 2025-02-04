389ab3224e31fd7b072c15f8d75fa7eb8f7ba331
JoseDamianRamirezToriz <jose.ramirez4842@alumnos.udg.mx>
2025-02-03 22:29:30 -0600

    probando cosas
    Ajustes del repositorio y probando codigos

--- Practica 1/ode45PenduloInvertido.m
+++ Practica 1/ode45PenduloInvertido.m
@@ -1,24 +1,45 @@
 %---Condiciones iniciales---%
-z0 = [0; 0; deg2rad(1); 0]; % [xc(0), dxc(0), alpha(0), dalpha(0)]
+z0 = [0, 0, 1, 0]; % [xc(0), dxc(0), alpha(0), dalpha(0)]
 
 %---Tiempo de simulación---%
-tspan = [0 10]; % De 0 a 10 segundos
+tspan = [0,  10]; % De 0 a 10 segundos
 
 %---Resolver el sistema con ODE45---%
 [t, z] = ode45(@PenduloInvertido, tspan, z0);
 
 %---Graficar los resultados---%
 figure(1);
-subplot(2, 1, 1);
-plot(t, z(:, 1), 'b', 'LineWidth', 1.5);
-grid on;
-title("Posición del carro (x_c)");
-xlabel("Tiempo (s)");
-ylabel("Posición (m)");
+subplot(2, 1, 1); 
 
-subplot(2, 1, 2);
-plot(t, rad2deg(z(:, 3)), 'r', 'LineWidth', 1.5);
+% Posición del carrito
+plot(t, z(:, 1), 'b', 'LineWidth', 1.5); 
+hold on;
+
+% Velocidad del carrito
+plot(t, z(:, 2), 'r', 'LineWidth', 1.5);
+hold off;
+
+xlabel('Tiempo');
+ylabel('Carrito');
+title('Variables del Carrito');
+legend('Posición (z1)', 'Velocidad (z2)');
 grid on;
-title("Ángulo del péndulo (\alpha)");
-xlabel("Tiempo (s)");
-ylabel("Ángulo (°)");
+
+%-------------------------------------------------------
+
+% Variables del péndulo
+subplot(2, 1, 2); 
+
+% Ángulo del péndulo
+plot(t, z(:, 3), 'g', 'LineWidth', 1.5); 
+hold on;
+
+% Velocidad angular del péndulo
+plot(t, z(:, 4), 'm', 'LineWidth', 1.5); 
+hold off;
+
+xlabel('Tiempo (s)');
+ylabel('Péndulo');
+title('Variables del Péndulo');
+legend('Ángulo (z3)', 'Velocidad angular (z4)');
+grid on;
